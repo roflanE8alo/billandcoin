4297b644a1b2cfc55c77bc761935305c76d8ddfa
Maxim Shircko <maxim.shircko@yandex.ru>
2024-11-07 15:22:50 +0300

    Initial commit

---
+++ src/main/java/org/example/userregistrationapp/UserRegistrationApp.java
@@ -0,0 +1,11 @@
+package org.example.userregistrationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationApp {
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationApp.class, args);
+    }
+}
---
+++ src/main/java/org/example/userregistrationapp/controller/UserController.java
@@ -0,0 +1,26 @@
+package org.example.userregistrationapp.controller;
+
+import org.example.userregistrationapp.model.User;
+import org.example.userregistrationapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
